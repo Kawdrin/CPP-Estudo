7578f8225ff6f249b86a28c27930d8d4de7a3633
Kawdrin <kawan.inf@gmail.com>
2021-04-18 22:53:47 -0300

    Declarações múltiplas de variáveis, Constantes #Define

---
+++ aula5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+#define CABA "kawdrin"
+#define APRESENTA cout << "Kawan Apresenta\n\n"
+
+int main(){
+
+  int vidas=3, muni=100, life=120;
+  cout << CABA << "\n";
+  APRESENTA;
+
+  return 0;
+}
